67f01588ee0777874e86153ebe88d382e4488570
harshinialagesan <145484802+harshinialagesan@users.noreply.github.com>
2024-12-04 13:10:31 +0530

    Add files via upload

---
+++ social-sharing-app/src/main/java/com/project/socialsharingApp/social_sharing_app/SocialSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.socialsharingApp.social_sharing_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSharingAppApplication.class, args);
+	}
+
+}
---
+++ social-sharing-app/src/test/java/com/project/socialsharingApp/social_sharing_app/SocialSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.socialsharingApp.social_sharing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
